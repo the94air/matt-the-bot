c582c0979d8a2983de6bdff0bae90c8f64d32f87
Abdalla <the94air@users.noreply.github.com>
2019-11-05 19:49:41 +0200

    Big bang

---
+++ bot.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+require('dotenv').config();
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+
+client.login(process.env.SECRET);
+
+require('http').createServer().listen(3000)
