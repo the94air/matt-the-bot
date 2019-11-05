bbcfeab92cfd8289dec5e931c03850edb41850fc
Abdalla <the94air@users.noreply.github.com>
2019-11-05 21:09:05 +0200

    Ready for now

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const Discord = require('discord.js');
 require('dotenv').config();
+const p = '!';
 
 const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+
+  client.user.setPresence({
+    status: "online",
+    game: {
+      name: "a game only for bots!",
+      type: "PLAYING"
+    }
+  }); 
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
+  if (msg.content === `${p} ping`) {
     msg.reply('pong');
   }
 });
